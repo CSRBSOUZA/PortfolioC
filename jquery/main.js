92cc97051f161f4fdc2aa01a16822d6222c5b8d5
catiasouza <catiamsouza80@gmail.com>
2018-09-20 12:14:04 -0300

    Add files via upload

--- jquery/main.js
+++ jquery/main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 			$(function() {
 				var $imgs = $('#images .carousel'),
@@ -42,49 +41,4 @@
 					}
 				});
 			});
-=======
-
-			$(function() {
-				var $imgs = $('#images .carousel'),
-					$capt = $('#captions .carousel'),
-					$timr = $('#timer');
-
-				$imgs.carouFredSel({
-					circular: false,
-					scroll: {
-						easing: 'quadratic',
-						duration: 2,
-						timeoutDuration: 3000,
-						onBefore: function( data ) {
-							$capt.trigger( 'slideTo', [ '.' + data.items.visible.first().attr( 'alt' ) ] );
-							$timr.stop().animate({
-								opacity: 0
-							}, data.scroll.duration);
-						},
-						onAfter: function() {
-							$timr.stop().animate({
-								opacity: 1
-							}, 150);
-						}
-					},
-					auto: {
-						progress: '#timer'
-					},
-					pagination: {
-						container: '.pager',
-						anchorBuilder: function( i ) {
-							return '<a class="p' + i + '" href="#"></a>';
-						}
-					}
-				});
-				$capt.carouFredSel({
-					circular: false,
-					auto: false,
-					scroll: {
-						easing: 'quadratic',
-						duration: 2
-					}
-				});
-			});
->>>>>>> a12296488e5241a4e9cd413bc0c5c48f24a00ff6
 		
